298523d05858c48512dc901941251b0b14377413
Teplov <vladimir.teplov@gmail.com>
2022-03-12 18:38:46 +0200

    add user migration, makefile command, config dist files; update .gitignore

---
+++ api/database/migrations/20220312_182028_add_user_table.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"github.com/beego/beego/v2/client/orm/migration"
+)
+
+// DO NOT MODIFY
+type AddUserTable_20220312_182028 struct {
+	migration.Migration
+}
+
+// DO NOT MODIFY
+func init() {
+	m := &AddUserTable_20220312_182028{}
+	m.Created = "20220312_182028"
+
+	migration.Register("AddUserTable_20220312_182028", m)
+}
+
+// Run the migrations
+func (m *AddUserTable_20220312_182028) Up() {
+	m.SQL("CREATE TABLE user (" +
+		"id int NOT NULL PRIMARY KEY AUTO_INCREMENT, " +
+		"email varchar(255), " +
+		"password text, " +
+		"salt text);")
+	m.SQL("ALTER TABLE user ADD UNIQUE INDEX email_idx(email);")
+}
+
+// Reverse the migrations
+func (m *AddUserTable_20220312_182028) Down() {
+	// use m.SQL("DROP TABLE ...") to reverse schema update
+
+}
--- api/go.mod
+++ api/go.mod
@@ -4,4 +4,7 @@ go 1.13
 
 require github.com/beego/beego/v2 v2.0.1
 
-require github.com/smartystreets/goconvey v1.6.4
+require (
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/smartystreets/goconvey v1.6.4
+)
