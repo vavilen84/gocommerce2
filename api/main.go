77f00228922c9cbea3923bc33b98fb8c20f4f104
Teplov <vladimir.teplov@gmail.com>
2022-03-12 20:32:52 +0200

    add logger

--- api/main.go
+++ api/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	_ "api/routers"
-
+	"github.com/beego/beego/v2/core/logs"
 	beego "github.com/beego/beego/v2/server/web"
 )
 
 func main() {
+	loggerCfg := `{"filename":"project.log","level":7,"maxlines":0,"maxsize":0,"daily":true,"maxdays":10,"color":true}`
+	logs.SetLogger(logs.AdapterFile, loggerCfg)
 	if beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
