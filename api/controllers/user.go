932362e3f0d62aecfe64f8efe8a56c5ab394abba
Teplov <vladimir.teplov@gmail.com>
2022-04-22 16:39:16 +0300

    user controller: comment methods bodies

--- api/controllers/user.go
+++ api/controllers/user.go
@@ -1,9 +1,6 @@
 package controllers
 
 import (
-	"api/models"
-	"encoding/json"
-
 	beego "github.com/beego/beego/v2/server/web"
 )
 
@@ -19,11 +16,11 @@ type UserController struct {
 // @Failure 403 body is empty
 // @router / [post]
 func (u *UserController) Post() {
-	var user models.User
-	json.Unmarshal(u.Ctx.Input.RequestBody, &user)
-	uid := models.AddUser(user)
-	u.Data["json"] = map[string]string{"uid": uid}
-	u.ServeJSON()
+	//var user models.User
+	//json.Unmarshal(u.Ctx.Input.RequestBody, &user)
+	//uid := models.AddUser(user)
+	//u.Data["json"] = map[string]string{"uid": uid}
+	//u.ServeJSON()
 }
 
 // @Title GetAll
@@ -31,9 +28,9 @@ func (u *UserController) Post() {
 // @Success 200 {object} models.User
 // @router / [get]
 func (u *UserController) GetAll() {
-	users := models.GetAllUsers()
-	u.Data["json"] = users
-	u.ServeJSON()
+	//users := models.GetAllUsers()
+	//u.Data["json"] = users
+	//u.ServeJSON()
 }
 
 // @Title Get
@@ -43,16 +40,16 @@ func (u *UserController) GetAll() {
 // @Failure 403 :uid is empty
 // @router /:uid [get]
 func (u *UserController) Get() {
-	uid := u.GetString(":uid")
-	if uid != "" {
-		user, err := models.GetUser(uid)
-		if err != nil {
-			u.Data["json"] = err.Error()
-		} else {
-			u.Data["json"] = user
-		}
-	}
-	u.ServeJSON()
+	//uid := u.GetString(":uid")
+	//if uid != "" {
+	//	user, err := models.GetUser(uid)
+	//	if err != nil {
+	//		u.Data["json"] = err.Error()
+	//	} else {
+	//		u.Data["json"] = user
+	//	}
+	//}
+	//u.ServeJSON()
 }
 
 // @Title Update
@@ -63,18 +60,18 @@ func (u *UserController) Get() {
 // @Failure 403 :uid is not int
 // @router /:uid [put]
 func (u *UserController) Put() {
-	uid := u.GetString(":uid")
-	if uid != "" {
-		var user models.User
-		json.Unmarshal(u.Ctx.Input.RequestBody, &user)
-		uu, err := models.UpdateUser(uid, &user)
-		if err != nil {
-			u.Data["json"] = err.Error()
-		} else {
-			u.Data["json"] = uu
-		}
-	}
-	u.ServeJSON()
+	//uid := u.GetString(":uid")
+	//if uid != "" {
+	//	var user models.User
+	//	json.Unmarshal(u.Ctx.Input.RequestBody, &user)
+	//	uu, err := models.UpdateUser(uid, &user)
+	//	if err != nil {
+	//		u.Data["json"] = err.Error()
+	//	} else {
+	//		u.Data["json"] = uu
+	//	}
+	//}
+	//u.ServeJSON()
 }
 
 // @Title Delete
@@ -84,10 +81,10 @@ func (u *UserController) Put() {
 // @Failure 403 uid is empty
 // @router /:uid [delete]
 func (u *UserController) Delete() {
-	uid := u.GetString(":uid")
-	models.DeleteUser(uid)
-	u.Data["json"] = "delete success!"
-	u.ServeJSON()
+	//uid := u.GetString(":uid")
+	//models.DeleteUser(uid)
+	//u.Data["json"] = "delete success!"
+	//u.ServeJSON()
 }
 
 // @Title Login
@@ -98,14 +95,14 @@ func (u *UserController) Delete() {
 // @Failure 403 user not exist
 // @router /login [get]
 func (u *UserController) Login() {
-	username := u.GetString("username")
-	password := u.GetString("password")
-	if models.Login(username, password) {
-		u.Data["json"] = "login success"
-	} else {
-		u.Data["json"] = "user not exist"
-	}
-	u.ServeJSON()
+	//username := u.GetString("username")
+	//password := u.GetString("password")
+	//if models.Login(username, password) {
+	//	u.Data["json"] = "login success"
+	//} else {
+	//	u.Data["json"] = "user not exist"
+	//}
+	//u.ServeJSON()
 }
 
 // @Title logout
@@ -113,6 +110,6 @@ func (u *UserController) Login() {
 // @Success 200 {string} logout success
 // @router /logout [get]
 func (u *UserController) Logout() {
-	u.Data["json"] = "logout success"
-	u.ServeJSON()
+	//u.Data["json"] = "logout success"
+	//u.ServeJSON()
 }
